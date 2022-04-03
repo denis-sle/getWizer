880aa787672cd7478f86e1a72432a25b5907e788
Denis Slepichev <slepichev@gmail.com>
2022-04-03 17:30:05 +0300

    Update test_app.py

--- backend/test_app.py
+++ backend/test_app.py
@@ -3,7 +3,7 @@ from app import choiceCalc as Calculator
 
 class TestCalc(unittest.TestCase):
     def test_calc(self):
-        self.assertEqual(Calculator([10, 20]), 10 | 20, "Should be 10 or 20")
+        self.assertIn(Calculator([10, 20]), [10, 20], "Should be 10 or 20")
 
 if __name__ == '__main__':
     unittest.main()
